f80d74216704055c91bf7f18df94c5d9765d174f
hasnain haider <hasnainhaider2663@gmail.com>
2020-12-22 02:06:01 +0500

    it works!

--- functions/index.js
+++ functions/index.js
@@ -24,3 +24,21 @@ exports.createProfile = functions.auth.user().onCreate((user) => {
     return admin.firestore().doc('users/' + user.uid).set(userObject);
 
 });
+
+exports.createThumbnailFromAsset = functions.firestore
+    .document('assets/{assetId}')
+    .onCreate(async (snap, context) => {
+        // Get an object representing the document
+        // e.g. {'name': 'Marie', 'age': 66}
+        const asset = snap.data();
+        console.log('snap',snap)
+        console.log('context',context)
+
+        const file = functions.storage.bucket().file(`asset.thumbnailURL`);
+        const metaData = await file.getMetaData();
+        asset['thumbnails']['small'] = metaData[0].mediaLink
+        // access a particular field as you would any JS property
+
+    return     admin.firestore().doc('assets/' + asset.uid).set(asset);
+        // perform desired operations ...
+    });
